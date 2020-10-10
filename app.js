cfa7f828b41de3cfe40d6c41468b252c4008168c
Andrii Kylymnyk <a.kylymnyk@gmail.com>
2020-10-10 16:15:59 +0300

    web-app

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    console.log('This is callback for app.get("/")');
+    console.log(req.url);
+    // res.send({ name: 'mango' });
+    res.send('Hi, this is "/"');
+
+});
+app.get('/about', (req, res) => {
+    console.log('This is callback for app.get("/about")');
+    console.log(req.url);
+});
+
+app.listen(1988, () => console.log(`Application servsr is running on port ${1988}`));
